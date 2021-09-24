08ee27a674525518f4cc5d085a7cafa644e332b7
xiaobo.cao <xiaobo.cao@dianping.com>
2021-09-24 20:31:34 +0800

    fix

--- chooseFloor/urls.py
+++ chooseFloor/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('index/', views.index),
     path('', views.index),
     path('chooseFloor/', views.index),
-    path('setFloor/', views.setFloor),
+    path('chooseFloor/setFloor/', views.setFloor),
 ]
